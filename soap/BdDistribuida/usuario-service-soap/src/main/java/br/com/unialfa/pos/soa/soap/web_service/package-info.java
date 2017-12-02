9c95dfc00a68e8d5a377e071eeeb847719bba398
Juliano <juliano.brasil@gmail.com>
2017-12-02 16:55:11 -0200

    Atualização do plugin do jaxb2 (#14)

--- soap/BdDistribuida/usuario-service-soap/src/main/java/br/com/unialfa/pos/soa/soap/web_service/package-info.java
+++ soap/BdDistribuida/usuario-service-soap/src/main/java/br/com/unialfa/pos/soa/soap/web_service/package-info.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2017.11.28 às 10:27:12 PM BRST 
+// Gerado em: 2017.12.02 às 04:45:55 PM BRST 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://soa.pos.unialfa.com.br/soap/web-service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
